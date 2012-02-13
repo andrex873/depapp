a2b0eb69656e7df99fb8c1d69408fab8a05e3bbd
admin-git <admi-git@dominio.com>
2012-02-13 16:44:16 -0500

    Ajustes login y base del proyecto.

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -2,7 +2,12 @@
 
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
+    
+    protected function _initLoadSession(){ 
+        $session = new Zend_Session_Namespace('global');                        
+        if(isset($session->logueado)){ 
+        }
+    }
 
 }
 
--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,16 +3,42 @@
 class IndexController extends Zend_Controller_Action
 {
 
+    private $sesion = null;
+
     public function init()
     {
-        /* Initialize action controller here */
+        $this->sesion = new Zend_Session_Namespace('global');
     }
 
     public function indexAction()
+    {
+        
+    }
+
+    public function loginAction()
+    {   
+        $user = "amendez";
+        $pwd = "123";
+        $post = $this->getRequest()->getPost();
+        if($user == $post['in_user'] && $pwd == $post['in_pass']){
+            $this->session->logueado = true;            
+            $this->_forward("inicio");
+        }else{
+            $this->_forward("index");        
+        }
+    }
+
+    public function logoutAction()
+    {
+        if (isset($this->session->logueado)) 
+            Zend_Session::destroy (true);            
+        $this->_redirect("/index/index");
+    }
+
+    public function inicioAction()
     {
         // action body
     }
 
 
 }
-
