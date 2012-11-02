1e9f1bfe83660c3b3ea5f53327d3b61a50c8af03
amendez <amj.desarrollo@gmail.com>
2012-11-02 12:08:43 -0500

    Se grega método getAllClaveValor()

--- application/models/Cargos.php
+++ application/models/Cargos.php
@@ -4,6 +4,19 @@ class Application_Model_Cargos extends Zend_Db_Table_Abstract
 {
     protected $_name = 't_cargos';
     protected $_primary = 'idCargo';        
-
+    
+    /**
+     * Metodo que obtiene todos los registros de la tabla y los retorna en parejas clave-valor.
+     * @param String $estado Estado de los registros que se quieren obtener, por defecto trae los ACT.
+     * @return Array Array con los registros de la tabla. 
+     */
+    public function getAllClaveValor($estado = 'ACT') {
+        $cargos = array();
+        $filas = $this->fetchAll("estadoCargo = '{$estado}'");
+        foreach ($filas as $cargo) {
+            $cargos[$cargo->idCargo] = $cargo->nombreCargo;
+        }
+        return $cargos;
+    }
 }
 
--- application/models/TipoContratos.php
+++ application/models/TipoContratos.php
@@ -4,6 +4,19 @@ class Application_Model_TipoContratos extends Zend_Db_Table_Abstract
 {
     protected $_name = 't_tipo_contratos';
     protected $_primary = 'idTipoContrato';        
-
+    
+    /**
+     * Metodo que obtiene todos los registros de la tabla y los retorna en parejas clave-valor.
+     * @param String $estado Estado de los registros que se quieren obtener, por defecto trae los ACT.
+     * @return Array Array con los registros de la tabla.  
+     */
+    public function getAllClaveValor($estado = 'ACT') {
+        $tipoContratos = array();
+        $filas = $this->fetchAll("estadoTipoContrato = '{$estado}'");
+        foreach ($filas as $tipoContrato) {
+            $tipoContratos[$tipoContrato->idTipoContrato] = $tipoContrato->nombreTipoContrato;
+        }
+        return $tipoContratos;
+    }
 }
 
